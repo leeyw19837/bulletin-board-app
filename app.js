c2e556dfdb279e9a03c4c7c38e284d991423fd39
liyanwen <leeyw19837@hotmail.com>
2020-04-09 09:47:15 +0800

    test docker auto build

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ new Vue({
     },
 
     deleteEvent: function (id) {
-      if (confirm('Are you sure you want to delete this event?')) {        
+      if (confirm('Are you sure you want to delete this event hahahahaha?')) {
         this.$http.delete('api/events/' + id)
           .success(function (res) {
             console.log(res);
